74aa182fe652d74952aa3fc8562896ff6b625e68
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-07-17 11:39:59 +0300

    Update create-signed-env.sh
    Signed-off-by: tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -43,22 +43,23 @@ mkdir ~/.android-certs
 
 # Passwordless certificates
 
-for x in bluetooth cyngn-app media networkstack nfc platform releasekey sdk_sandbox shared testcert testkey verity verifiedboot; do \
+# failed rebooted in fastboot
+#for x in bluetooth cyngn-app media networkstack nfc platform releasekey sdk_sandbox shared testcert testkey verity verifiedboot; do \
+#    printf "\n\n" | ./development/tools/make_key ~/.android-certs/$x "$subject"; \
+#done
+
+for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do \
     printf "\n\n" | ./development/tools/make_key ~/.android-certs/$x "$subject"; \
 done
 
-#for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do \
-#    ./development/tools/make_key ~/.android-certs/$x "$subject" > /dev/null 2>&1 && printf "\n\n" \
+#cp ./development/tools/make_key ~/.android-certs/
+#sed -i 's|2048|4096|g' ~/.android-certs/make_key
+
+
+#for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do \
+#    printf "\n\n" | ~/.android-certs/make_key ~/.android-certs/$apex "$subject"; \
+#    openssl pkcs8 -in ~/.android-certs/$apex.pk8 -inform DER -nocrypt -out ~/.android-certs/$apex.pem; \
 #done
-
-cp ./development/tools/make_key ~/.android-certs/
-sed -i 's|2048|4096|g' ~/.android-certs/make_key
-
-
-for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do \
-    printf "\n\n" | ~/.android-certs/make_key ~/.android-certs/$apex "$subject"; \
-    openssl pkcs8 -in ~/.android-certs/$apex.pk8 -inform DER -nocrypt -out ~/.android-certs/$apex.pem; \
-done
 
 ## Create vendor for keys
 mkdir -p vendor/lineage-priv
